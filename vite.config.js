8d9340d9fe4d386d967b98f09272fc5ddcdf2864
Mahyar <mahyar.shaabani@yahoo.com>
2025-05-13 03:33:40 +0200

    update base path for static website in vite.config.js and add CNAME file

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import {defineConfig} from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-    base: '/mahyar-website-static/',
+    base: '/',
     plugins: [
         tailwindcss(),
     ],
